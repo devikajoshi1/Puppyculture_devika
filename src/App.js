06bc4c322cb7ef133e2dc995c1ce4c0e70db1085
Atharv1903 <awishkar193@gmail.com>
2024-01-17 08:52:55 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Component from "./components/component3/component1";
+//import Component from "./components/component3/component1";
 import Component4 from "./components/component4/component4";
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
       
       
       <Routes>
-        <Route path="/c3" element={<Component/>}/>
+        {/* <Route path="/c3" element={<Component/>}/> */}
         <Route path="/c4" element={<Component4/>}/>
       </Routes>
     </>

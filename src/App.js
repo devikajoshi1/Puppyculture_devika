13db4e33af1e3b0f93f03f52d33ccae52a260bb0
Atharv1903 <awishkar193@gmail.com>
2024-01-18 14:30:20 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-//import Component from "./components/component3/component1";
+import Component from "./components/component3/component1";
 import Component4 from "./components/component4/component4";
 
 const App = () => {
   return (
     <>
       <Routes>
-        {/* <Route path="/c3" element={<Component/>}/> */}
+        <Route path="/c3" element={<Component/>}/>
         <Route path="/c4" element={<Component4/>}/>
       </Routes>
     </>

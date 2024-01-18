f67db7127c425e7adc0cfc9db52da785152d0b8d
Atharv1903 <awishkar193@gmail.com>
2024-01-18 14:11:36 +0530

    change

--- src/Layout/Footer/footer.jsx
+++ src/Layout/Footer/footer.jsx
@@ -4,112 +4,6 @@ import './footer1.css';
 
 const Footer = () => {
   return (
-    // <div class="footer color-background-2 gradient section-sections--14346363404382__footer-padding">
-    //   <div class="footer__content-top page-width">
-    //     <div class="footer__blocks-wrapper grid grid--1-col grid--2-col grid--4-col-tablet">
-    //       <div class="footer-block grid__item footer-block--menu">
-    //         <h2 class="footer-block__heading">Quick links</h2>
-    //         <li class="footer-block__details-content list-unstyled">
-    //           <ul>
-    //             <a
-    //               href="https://shoppuppyculture.com/account"
-    //               class="link link--text list-menu__item list-menu__item--link"
-    //             >
-    //               My Account
-    //             </a>
-    //           </ul>
-    //           <ul>
-    //             <a
-    //               href="https://shoppuppyculture.com/pages/support?url=https://support.shoppuppyculture.com/support/solutions"
-    //               class="link link--text list-menu__item list-menu__item--link"
-    //             >
-    //               Support Portal / FAQs
-    //             </a>
-    //           </ul>
-    //           <ul>
-    //             <a
-    //               //href="/pages/keep-informed"
-    //               class="link link--text list-menu__item list-menu__item--link"
-    //             >
-    //               Keep Informed
-    //             </a>
-    //           </ul>
-    //           <ul>
-    //             <a
-    //               href="https://support.shoppuppyculture.com/support/home"
-    //               class="link link--text list-menu__item list-menu__item--link"
-    //             >
-    //               Contact Us
-    //             </a>
-    //           </ul>
-    //           <ul>
-    //             <a
-    //               //href="/pages/legal-stuff"
-    //               class="link link--text list-menu__item list-menu__item--link"
-    //             >
-    //               Policies / Data Requests
-    //             </a>
-    //           </ul>
-    //         </li>
-    //       </div>
-    //     </div>
-    //     <div class="footer-block--newsletter"></div>
-    //   </div>
-    //   <div class="footer__content-bottom">
-    //     <div class="footer__content-bottom-wrapper page-width">
-    //       <div class="footer__column footer__localization isolate"></div>
-    //       <div class="footer__column footer__column--info"></div>
-    //     </div>
-    //     <div class="footer__content-bottom-wrapper page-width footer__content-bottom-wrapper--center">
-    //       <div class="footer__copyright caption">
-    //         <ul class="policies list-unstyled">
-    //           <li>
-    //           <small class="copyright__content">© 2023, 
-    //           <a href="/" title="">PUPPY CULTURE</a></small>
-    //           <small class="copyright__content">
-    //           <a
-    //           target="_blank"
-    //           rel="nofollow"
-    //           href="https://www.shopify.com?utm_campaign=poweredby&amp;utm_medium=shopify&amp;utm_source=onlinestore"
-    //           ></a></small>
-    //           </li>
-    //           <li>
-    //             <small class="copyright__content"
-    //             ><a href="/policies/refund-policy">Return/Refund policy</a></small
-    //             >
-    //           </li>
-    //           <li>
-    //             <small class="copyright__content"
-    //             ><a href="/policies/privacy-policy">Privacy policy</a></small
-    //             >
-    //           </li>
-    //           <li>
-    //             <small class="copyright__content"
-    //             ><a href="/policies/terms-of-service">Terms of service</a></small
-    //             >
-    //           </li>
-    //           <li>
-    //             <small class="copyright__content"
-    //             ><a href="/policies/shipping-policy">Shipping policy</a></small
-    //             >
-    //           </li>
-    //         </ul>
-    //         <div class="grid grid--no-gutters">
-    //           <div class="ftrcntr">
-    //             <hr />
-    //             <img
-    //               src="https://cdn.shopify.com/s/files/1/1554/7897/files/PC-PROOF-IS-IN-THE-PUPPIES-reg-trademark.png?14941266797717180377"
-    //             />
-    //             <img
-    //               src="https://cdn.shopify.com/s/files/1/1554/7897/files/Madcap-UP-zer0-bg-75x150.png?v=1576798821"
-    //             />
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
       <div class="footer color-background-2 gradient section-sections--14346363404382__footer-padding">
         <div class="footer__content-top page-width">
           <div
@@ -223,13 +117,6 @@ const Footer = () => {
                     src="https://cdn.shopify.com/s/files/1/1554/7897/files/Madcap-UP-zer0-bg-75x150.png?v=1576798821"
                   />
 
-                  {/* <!--
-                  <div id="m-1671121515421">
-                    <a href="https://play.google.com/store/apps/details?id=tv.uscreen.puppyculture&amp;pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Get it on Google Play" src="https://cdn.shopify.com/s/files/1/0360/4922/3818/files/googleplay-image.png?v=1670208110"></a>
-
-                    <a href="https://apps.apple.com/us/app/puppy-culture/id6443569865?itsct=apps_box_badge&amp;itscg=30200"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1665532800?h=787ae4514f03be53939eece7de396b19" alt="Download on the App Store"></a>
-                  </div>
-                  --> */}
                 </div>
               </div>
             </div>
@@ -238,17 +125,9 @@ const Footer = () => {
         <div>
         <div
           class="locale-selectors__container"
-          // style="background-color: rgb(17, 34, 54)"
         >
           <div
             class="locale-selectors__content"
-            // style="
-            //   max-width: 1500px;
-            //   margin-left: 0px;
-            //   margin-right: 0px;
-            //   padding-left: 62.5px;
-            //   padding-right: 62.5px;
-            // "
           >
             <form action="/localization" method="POST">
               <input name="_method" type="hidden" value="PUT" /><input
@@ -286,7 +165,7 @@ const Footer = () => {
                   <option value="BY">Belarus (USD $)</option>
                   <option value="BE">Belgium (EUR €)</option>
                   <option value="BZ">Belize (BZD $)</option>
-                  <option value="BJ">Benin (XOF F CFA)</option>
+                  <option value="BJ">Benin (XOF FCFA)</option>
                   <option value="BM">Bermuda (USD $)</option>
                   <option value="BT">Bhutan (USD $)</option>
                   <option value="BO">Bolivia (BOB Bs)</option>
@@ -297,7 +176,7 @@ const Footer = () => {
                   <option value="VG">British Virgin Islands (USD $)</option>
                   <option value="BN">Brunei (BND $)</option>
                   <option value="BG">Bulgaria (BGN)</option>
-                  <option value="BF">Burkina Faso (XOF F CFA)</option>
+                  <option value="BF">Burkina Faso (XOF FCFA)</option>
                   <option value="BI">Burundi (BIF)</option>
                   <option value="KH">Cambodia (KHR ៛)</option>
                   <option value="CM">Cameroon (XAF FCFA)</option>
@@ -317,7 +196,7 @@ const Footer = () => {
                   <option value="CD">Congo - Kinshasa (CDF)</option>
                   <option value="CK">Cook Islands (NZD $)</option>
                   <option value="CR">Costa Rica (CRC ₡)</option>
-                  <option value="CI">Côte d’Ivoire (XOF F CFA)</option>
+                  <option value="CI">Côte d'Ivoire (XOF FCFA)</option>
                   <option value="HR">Croatia (EUR €)</option>
                   <option value="CW">Curaçao (ANG)</option>
                   <option value="CY">Cyprus (EUR €)</option>
@@ -342,7 +221,7 @@ const Footer = () => {
                   <option value="GF">French Guiana (EUR €)</option>
                   <option value="PF">French Polynesia (XPF CFPF)</option>
                   <option value="TF">French Southern Territories (EUR €)</option>
-                  <option value="GA">Gabon (XOF F CFA)</option>
+                  <option value="GA">Gabon (XOF FCFA)</option>
                   <option value="GM">Gambia (GMD)</option>
                   <option value="GE">Georgia (USD $)</option>
                   <option value="DE">Germany (EUR €)</option>
@@ -355,7 +234,7 @@ const Footer = () => {
                   <option value="GT">Guatemala (GTQ Q)</option>
                   <option value="GG">Guernsey (GBP £)</option>
                   <option value="GN">Guinea (GNF FG)</option>
-                  <option value="GW">Guinea-Bissau (XOF F CFA)</option>
+                  <option value="GW">Guinea-Bissau (XOF FCFA)</option>
                   <option value="GY">Guyana (GYD $)</option>
                   <option value="HT">Haiti (USD $)</option>
                   <option value="HN">Honduras (HNL L)</option>
@@ -393,7 +272,7 @@ const Footer = () => {
                   <option value="MW">Malawi (MWK)</option>
                   <option value="MY">Malaysia (MYR RM)</option>
                   <option value="MV">Maldives (MVR)</option>
-                  <option value="ML">Mali (XOF F CFA)</option>
+                  <option value="ML">Mali (XOF FCFA)</option>
                   <option value="MT">Malta (EUR €)</option>
                   <option value="MQ">Martinique (EUR €)</option>
                   <option value="MR">Mauritania (USD $)</option>
@@ -415,7 +294,7 @@ const Footer = () => {
                   <option value="NC">New Caledonia (XPF CFPF)</option>
                   <option value="NZ">New Zealand (NZD $)</option>
                   <option value="NI">Nicaragua (NIO C$)</option>
-                  <option value="NE">Niger (XOF F CFA)</option>
+                  <option value="NE">Niger (XOF FCFA)</option>
                   <option value="NG">Nigeria (NGN ₦)</option>
                   <option value="NU">Niue (NZD $)</option>
                   <option value="NF">Norfolk Island (AUD $)</option>
@@ -441,7 +320,7 @@ const Footer = () => {
                   <option value="SM">San Marino (EUR €)</option>
                   <option value="ST">São Tomé &amp; Príncipe (STD)</option>
                   <option value="SA">Saudi Arabia (SAR)</option>
-                  <option value="SN">Senegal (XOF F CFA)</option>
+                  <option value="SN">Senegal (XOF FCFA)</option>
                   <option value="RS">Serbia (RSD)</option>
                   <option value="SC">Seychelles (USD $)</option>
                   <option value="SL">Sierra Leone (SLL)</option>
@@ -476,7 +355,7 @@ const Footer = () => {
                   <option value="TZ">Tanzania (TZS)</option>
                   <option value="TH">Thailand (THB ฿)</option>
                   <option value="TL">Timor-Leste (USD $)</option>
-                  <option value="TG">Togo (XOF F CFA)</option>
+                  <option value="TG">Togo (XOF FCFA)</option>
                   <option value="TK">Tokelau (NZD $)</option>
                   <option value="TO">Tonga (TOP T$)</option>
                   <option value="TT">Trinidad &amp; Tobago (TTD $)</option>
